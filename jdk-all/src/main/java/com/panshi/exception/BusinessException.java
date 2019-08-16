6ebc34d84239ad5da9e347832153aca572e67e4e
dengjunhao <dengjunhao008@qq.com>
2019-08-16 14:42:38 +0800

    添加故障接口

---
+++ jdk-all/src/main/java/com/panshi/exception/BusinessException.java
@@ -0,0 +1,15 @@
+package com.panshi.exception;
+
+import lombok.Data;
+
+@Data
+public class BusinessException extends RuntimeException{
+ private Integer code;
+
+ private String message;
+
+ public BusinessException(Integer code, String message) {
+    this.code = code;
+    this.message = message;
+ }
+}
